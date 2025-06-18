92d23c3a9ce2c72f00de5b1528f2a404bbaf0c9a
AlanHSS <alanhsoaress@gmail.com>
2025-06-18 17:13:15 -0300

    first commit

---
+++ src/test/java/com/example/SistemaDeCadastro/SistemaDeCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaDeCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
